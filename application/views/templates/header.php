4374fceccd11882979dfd761205a7135797df5ed
justindbalaguer <49123646+justindbalaguer@users.noreply.github.com>
2019-04-15 22:59:35 +0800

    Add files via upload

---
+++ application/views/templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/darkly/bootstrap.min.css">
+    <title>codeigniter</title>
+  </head>
+  <body>
+      <nav class="navbar navbar-inverse">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="<?php echo base_url();?>">codeigniter</a>
+          </div>
+          <div id="navbar">
+            <ul class="nav navbar-nav">
+              <li><a href="<?php echo base_url();?>">Home</a></li>
+              <li><a href="<?php echo base_url();?>about">About</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+<div class="container">
